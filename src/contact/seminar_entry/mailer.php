9fe445340c3e7fbbc7be136ca6e143fb8776c723
ryoshioka <ryoshioka@iimhs.co.jp>
2019-02-05 15:03:53 +0900

    学校名の入力必須を解除、応募メールに備考が表示されるように対応

--- src/contact/seminar_entry/mailer.php
+++ src/contact/seminar_entry/mailer.php
@@ -32,11 +32,11 @@ $email->addTo($emailadd)->
        setFrom($from)->
        setFromName("IIMヒューマン・ソリューション株式会社")->
        setSubject("【IHS】エントリーありがとうございます" )->
-       setText(" $name 様\r\n\r\nこの度はIIMヒューマン・ソリューションの『情報処理技術の基本&業務プロセスセミナー』へご応募くださり、\r\n誠にありがとうございます。\r\n採用担当より折り返しご連絡いたしますので、今しばらくお待ちください。\r\n\r\n 氏名: $name \r\n フリガナ: $phonetic \r\n 性別: $sex \r\n 年齢: $age 歳 \r\n 電話番号: $phone1 - $phone2 -$phone3 \r\n メールアドレス: $emailadd \r\n 現在の状態: $status \r\n 業種: $status_other \r\n 学校名: $last_school \r\n\r\n※本メールは自動送信されています。\r\nお問い合わせがある場合は、saiyou@iimhs.co.jpまでご連絡ください。")->
+       setText(" $name 様\r\n\r\nこの度はIIMヒューマン・ソリューションの『情報処理技術の基本&業務プロセスセミナー』へご応募くださり、\r\n誠にありがとうございます。\r\n採用担当より折り返しご連絡いたしますので、今しばらくお待ちください。\r\n\r\n 氏名: $name \r\n フリガナ: $phonetic \r\n 性別: $sex \r\n 年齢: $age 歳 \r\n 電話番号: $phone1 - $phone2 -$phone3 \r\n メールアドレス: $emailadd \r\n 現在の状態: $status \r\n 業種: $status_other \r\n 学校名: $last_school \r\n 備考: $remarks \r\n\r\n※本メールは自動送信されています。\r\nお問い合わせがある場合は、saiyou@iimhs.co.jpまでご連絡ください。")->
 
 
 
-       setHtml(" $name 様<br /><br />この度はIIMヒューマン・ソリューションの『情報処理技術の基本&業務プロセスセミナー』へご応募くださり、<br />誠にありがとうございます。<br />採用担当より折り返しご連絡いたしますので、今しばらくお待ちください。<br /><br /> 氏名: $name <br /> フリガナ: $phonetic <br /> 性別: $sex <br /> 年齢: $age 歳 <br /> 電話番号: $phone1 - $phone2 -$phone3 <br /> メールアドレス: $emailadd <br /> 現在の状態: $status <br /> 業種: $status_other <br /> 学校名: $last_school <br /><br />※本メールは自動送信されています。<br />お問い合わせがある場合は、saiyou@iimhs.co.jpまでご連絡ください。")->
+       setHtml(" $name 様<br /><br />この度はIIMヒューマン・ソリューションの『情報処理技術の基本&業務プロセスセミナー』へご応募くださり、<br />誠にありがとうございます。<br />採用担当より折り返しご連絡いたしますので、今しばらくお待ちください。<br /><br /> 氏名: $name <br /> フリガナ: $phonetic <br /> 性別: $sex <br /> 年齢: $age 歳 <br /> 電話番号: $phone1 - $phone2 -$phone3 <br /> メールアドレス: $emailadd <br /> 現在の状態: $status <br /> 業種: $status_other <br /> 学校名: $last_school <br /> 備考: $remarks <br /><br />※本メールは自動送信されています。<br />お問い合わせがある場合は、saiyou@iimhs.co.jpまでご連絡ください。")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
@@ -49,10 +49,10 @@ $email->addTo($to)->
        setFrom($from)->
        setFromName("IHS")->
        setSubject("【『情報処理技術の基本&業務プロセスセミナー』へのエントリーがありました】" )->
-       setText("下記内容にて『情報処理技術の基本&業務プロセスセミナー』へのお申込みを受け付けました。\r\nご担当者は確認後、受付メールを送付してください。\r\n\r\n氏名: $name \r\n フリガナ: $phonetic \r\n 性別: $sex \r\n 年齢: $age 歳 \r\n 電話番号: $phone1 - $phone2 -$phone3 \r\n メールアドレス: $emailadd \r\n 現在の状態: $status \r\n 業種: $status_other \r\n 学校名: $last_school")->
+       setText("下記内容にて『情報処理技術の基本&業務プロセスセミナー』へのお申込みを受け付けました。\r\nご担当者は確認後、受付メールを送付してください。\r\n\r\n氏名: $name \r\n フリガナ: $phonetic \r\n 性別: $sex \r\n 年齢: $age 歳 \r\n 電話番号: $phone1 - $phone2 -$phone3 \r\n メールアドレス: $emailadd \r\n 現在の状態: $status \r\n 業種: $status_other \r\n 学校名: $last_school \r\n 備考: $remarks")->
 
 
-       setHtml("下記内容にて『情報処理技術の基本&業務プロセスセミナー』へのお申込みを受け付けました。<br />ご担当者は確認後、受付メールを送付してください。<br /><br />氏名: $name <br /> フリガナ: $phonetic <br /> 性別: $sex <br /> 年齢: $age 歳 <br /> 電話番号: $phone1 - $phone2 -$phone3 <br /> メールアドレス: $emailadd <br /> 現在の状態: $status <br /> 業種: $status_other <br /> 学校名: $last_school ")->
+       setHtml("下記内容にて『情報処理技術の基本&業務プロセスセミナー』へのお申込みを受け付けました。<br />ご担当者は確認後、受付メールを送付してください。<br /><br />氏名: $name <br /> フリガナ: $phonetic <br /> 性別: $sex <br /> 年齢: $age 歳 <br /> 電話番号: $phone1 - $phone2 -$phone3 <br /> メールアドレス: $emailadd <br /> 現在の状態: $status <br /> 業種: $status_other <br /> 学校名: $last_school<br /> 備考: $remarks <br /><br />")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
