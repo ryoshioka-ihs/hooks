3123da150088d5398156e8b377a3d9c56fe0fb17
aiji-0522 <growth0522@gmail.com>
2020-09-14 13:33:48 +0900

    修正しました。

--- src/js/seminar.js
+++ src/js/seminar.js
@@ -207,22 +207,23 @@ var seminarDate = document.getElementById( "seminardate") ;//id="seminardate"を
 	   seminarDate.options[1].disabled = true;//trueで日程を非表示でリセットする;
      } else if ( courseelement.value == "【MS365】PowerShellを利用したユーザアカウント管理方法 (第1回目)") {
 	   seminarDate.options[2].disabled = false;
-     } else if */( courseelement.value == "【MS365】Azureでのチャットボット機能の構築（第2回目）") {
-	   seminarDate.options[5].disabled = false;
-	 } else if ( courseelement.value == "【MS365】Azureでのチャットボット機能の構築（第3回目）") {
-	   seminarDate.options[7].disabled = false;
+     
      /*} else if ( courseelement.value == "【MS365】PowerAutomateを使用した日報作成 (条件分岐の設定方法）") {
 	   seminarDate.options[1].disabled = false;*/
-	 } else if ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第2回目)") {
+	 /*} else if*/ ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第2回目)") {
 	   seminarDate.options[1].disabled = false;
-	 } else if ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第3回目)") {
-	   seminarDate.options[3].disabled = false;
-	 } else if ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第4回目)") {
-	   seminarDate.options[6].disabled = false;
 	 } else if ( courseelement.value == "【MS365】PowerShellを利用したユーザアカウント管理方法 (第2回目)") {
 	   seminarDate.options[2].disabled = false;
+	 } else if ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第3回目)") {
+	   seminarDate.options[3].disabled = false;
 	 } else if ( courseelement.value == "【MS365】PowerShellを利用したユーザアカウント管理方法 (第3回目)") {
 	   seminarDate.options[4].disabled = false;
+	 } else if ( courseelement.value == "【MS365】Azureでのチャットボット機能の構築（第2回目）") {
+	   seminarDate.options[5].disabled = false;
+	 } else if ( courseelement.value == "【MS365】PowerAutomateを使用した申請業務 (第4回目)") {
+	   seminarDate.options[6].disabled = false;
+	 } else if ( courseelement.value == "【MS365】Azureでのチャットボット機能の構築（第3回目）") {
+	   seminarDate.options[7].disabled = false;
 	 } else if ( courseelement.value == "【MS365】PowerShellを利用したユーザアカウント管理方法 (第4回目)") {
 	   seminarDate.options[8].disabled = false;
 	 } else if ( courseelement.value = "") {
